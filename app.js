82d26aa5e13efb2cf07cc9e68b0253b1f2dbc37c
Mohammad Hammad <hammadsmitstudent126507@gmail.com>
2023-10-25 03:20:57 +0500

    Practice Purpose

---
+++ app.js
@@ -0,0 +1,108 @@
+// ============== Q No 1 ===============
+
+// var userInp =  prompt("enter first name")
+// var userInp2 =  prompt("enter last name")
+// var merge = (  ' ' + userInp + ' ' + userInp2)
+// alert("Hi," + merge)
+
+
+// ============== Q No 2 ===============
+
+// var userInp = prompt("enter you favourite phone model :")
+// document.write(userInp + "<br>" )
+// document.write( "Length of string is" + ' ' + userInp.length)
+
+// ============== Q No 3 ===============
+
+// var str = "PAKISTANI"
+// document.write(str+ "<br>")
+// document.write("Index Of  N is"+ ":" + " " + str.indexOf("T")  )
+
+
+// ============== Q No 4 ===============
+
+// var str = "Hello World"
+// document.write(str+ "<br>")
+// document.write("last index Of  'l' is"+ ":" + " " + str.lastIndexOf("l")  )
+
+
+// ============== Q No 5 ===============
+
+// var str = "PAKISTANI"
+// document.write(str+ "<br>")
+// document.write("Character at  Index 3 is"+ ":" + " " + str.charAt(3)  )
+
+// ============== Q No 6 ===============
+
+// var userInp =  prompt("enter first name")
+// var userInp2 =  prompt("enter last name")
+// var merge = (  ' ' + userInp + ' ' + userInp2)
+// alert("Hi," + merge)
+
+// ============== Q No 7 ===============
+
+// var city = "Hyderabad";
+// var replaced = city.replace("Hydera", "Islama");
+// document.write("City: " + city + "<br>After Replacement: " + replaced);
+
+// ============== Q No 8 ===============
+
+// var before = "Ali and Hammad are best friends" ;
+// var replaced = before.replace("and", "&" );
+// document.write( before + "<br>After Replacement: " + replaced);
+
+// ============== Q No 9 ===============
+
+// var string = "479";
+// document.write("String: " + string + "<br>Type: " + typeof(string), "<br>");
+// var num = parseInt(string);
+// document.write("Number: " + num + "<br>Type: " + typeof(num));
+
+// ============== Q No 10 ===============
+
+// var sent = "www.facbook.com"
+// var domain = sent.slice(4)
+// document.write( sent + "<br>" + domain)
+
+
+// ============== Q No 11 ===============
+
+// var userInp = prompt("Enter any word")
+//  var upper = userInp.toUpperCase(0)
+// document.write(upper)
+
+// ============== Q No 12 ===============
+
+// var userInp = prompt("Enter any word")
+//  var lower = userInp.toLowerCase(0)
+// document.write(lower)
+
+// ============== Q No 13 ===============
+
+// var userInput = prompt("Enter First Name: ");
+// var upperUserInput = userInput.charAt(0).toUpperCase();
+// var lowerUserInput = userInput.slice(1).toLowerCase();
+// var titled = upperUserInput + lowerUserInput
+
+// document.write("User input: " + userInput + "<br>Title case: " + titled);
+
+// ============== Q No 14 ===============
+
+// var num = 35.56
+// var after = num.toString();
+// var rep = after.replace ("." , "")
+// document.write(num + " " + rep)
+
+// 15th question -----------------------------------------------------------
+
+// var a = "3";
+// var b = "3";
+// document.write("a + b = " + a+b);
+
+
+// 16th question -----------------------------------------------------------
+
+// var a = 3;
+// var b = 3;
+// var res = (a - b)
+// document.write("a - b = " + res);
